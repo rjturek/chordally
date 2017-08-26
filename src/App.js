de784ba37166f6edc527899f1e857d10f7e86c23
Richard Turek <rjt1357@gmail.com>
2017-08-26 17:59:10 -0400

    test update

--- src/App.js
+++ src/App.js
@@ -11,8 +11,9 @@ class App extends Component {
           <h2>Welcome to React</h2>
         </div>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+	To get started, edit <code>src/App.js</code> and save to reload.
+	updizzled
+	</p>
       </div>
     );
   }
